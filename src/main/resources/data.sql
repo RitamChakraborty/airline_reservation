472318fe208fcc395932756e979d4624193d8b8b
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-01-17 06:33:58 +0530

    Specify column name for perfect data entry

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-insert into admin
+insert into admin(username, password)
 values ('ritam', 'password');
 
-insert into user
+insert into user(id, name, email, address, phone)
 values ('1001', 'Afrin Karasova', 'karasova.afrin@gmail.com', 'Saltlake City, UT', '9876543210'),
        ('1002', 'Kunal Goel', 'goel.kunal@gmail.com', 'Downtown Manhattan, LA', '8765432109'),
        ('1003', 'Pallavi Gupta', 'gupta.pallavi@gmail.com', 'Austin, Tx', '7654321098'),
