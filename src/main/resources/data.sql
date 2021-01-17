caea599ad1c07e75f6ce00d0362dfa99ee87e257
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-01-17 06:31:39 +0530

    Insert data in database for user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,8 @@
 insert into admin
 values ('ritam', 'password');
+
+insert into user
+values ('1001', 'Afrin Karasova', 'karasova.afrin@gmail.com', 'Saltlake City, UT', '9876543210'),
+       ('1002', 'Kunal Goel', 'goel.kunal@gmail.com', 'Downtown Manhattan, LA', '8765432109'),
+       ('1003', 'Pallavi Gupta', 'gupta.pallavi@gmail.com', 'Austin, Tx', '7654321098'),
+       ('1004', 'Shuktika Mahanty', 'mathanty.shuktika@gmail.com', 'Mountain View, CA', '6543210987');
