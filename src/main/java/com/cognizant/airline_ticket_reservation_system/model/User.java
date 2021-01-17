1efc1d5630dad65d8821123120201f6976b44e8b
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-01-17 06:22:07 +0530

    Create user model

---
+++ src/main/java/com/cognizant/airline_ticket_reservation_system/model/User.java
@@ -0,0 +1,57 @@
+package com.cognizant.airline_ticket_reservation_system.model;
+
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Component
+public class User {
+    @Id
+    private Integer id;
+    private String name;
+    private String email;
+    private String address;
+    private String phone;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
