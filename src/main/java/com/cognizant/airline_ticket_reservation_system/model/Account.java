c5d55c3f69da3dccea5c83cdf953f1e27f079642
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-01-25 20:32:47 +0530

    Add classes needed for payment

---
+++ src/main/java/com/cognizant/airline_ticket_reservation_system/model/Account.java
@@ -0,0 +1,7 @@
+package com.cognizant.airline_ticket_reservation_system.model;
+
+public class Account {
+    private Long accountNo;
+    private int balance;
+    private Integer bankId;
+}
---
+++ src/main/java/com/cognizant/airline_ticket_reservation_system/model/Bank.java
@@ -0,0 +1,6 @@
+package com.cognizant.airline_ticket_reservation_system.model;
+
+public class Bank {
+    private Integer id;
+    private String bankName;
+}
