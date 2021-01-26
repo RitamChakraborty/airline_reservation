ca5a609c51ce00d1277be1075b53e928e6ed811b
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-01-26 19:06:22 +0530

    Remove curd repositories for Bank and Account model

--- src/main/java/com/cognizant/airline_ticket_reservation_system/controller/AccountDao.java
+++
@@ -1,7 +0,0 @@
-package com.cognizant.airline_ticket_reservation_system.controller;
-
-import com.cognizant.airline_ticket_reservation_system.model.Account;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AccountDao extends CrudRepository<Account, Long> {
-}
