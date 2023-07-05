ca5ba61ada593ed981dca48adc0aee61545145b2
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-07-05 20:56:56 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,15 @@
+truncate table admin;
+
 insert into admin(username, password)
 values ('ritam', 'password');
 
+truncate table user;
+
 insert into user(password, name, age, gender, email, address, phone, secret_question, answer)
-values ('k.afrin', 'Afrin Kardashian', '22', 'Female', 'kardashian.afrin@gmail.com', 'Saltlake City, UT', '9876543210',
-        'Your nickname', 'afrin'),
-       ('g.kunal', 'Kunal Goel', '22', 'Male', 'goel.kunal@gmail.com', 'Downtown Manhattan, LA', '8765432109',
-        'Your nickname', 'kunal'),
-       ('g.pallavi', 'Pallavi Gupta', '22', 'Female', 'gupta.pallavi@gmail.com', 'Austin, Tx', '7654321098',
-        'Your nickname', 'pallavi'),
-       ('m.shuktika', 'Shuktika Mahanty', '22', 'Female', 'mathanty.shuktika@gmail.com', 'Mountain View, CA',
+values ('m.shuktika', 'Shuktika Mahanty', '22', 'Female', 'mathanty.shuktika@gmail.com', 'Mountain View, CA',
         '6543210987', 'Your nickname', 'shuktika');
+
+truncate table flight;
 
 insert into flight(airline, model, type, economy_seats, business_seats)
 values ('Air India', 'Airbus A300', 'Domestic', '50', '10'),
@@ -17,6 +17,8 @@ values ('Air India', 'Airbus A300', 'Domestic', '50', '10'),
        ('SpiceJet', 'Airbus A321', 'Domestic', '150', '50'),
        ('Emirates', 'Boeing 777', 'International', '200', '50'),
        ('British Airways', 'Boeing 787', 'International', '250', '75');
+
+truncate table flight_schedule;
 
 insert into flight_schedule(flight_no, source, destination, economy_seat_cost, business_seat_cost, arrival_time,
                             departure_time, sun, mon, tue, wed, thu,
@@ -26,15 +28,20 @@ values (1, 'Kolkata', 'Chennai', '3000', '5000', '10:00:00', '12:00:00', 1, 1, 1
        (4, 'Delhi', 'Dubai', '12000', '20000', '17:30:00', '19:00:00', 0, 1, 0, 1, 0, 1, 0),
        (5, 'Mumbai', 'London', '10000', '15000', '10:00:00', '13:00:00', 1, 0, 1, 0, 1, 0, 1);
 
-insert
-into news_feed(news, date)
+truncate table news_feed;
+
+insert into news_feed(news, date)
 values ('Due to fog some flights will get delayed', '2021-02-10'),
        ('Our agency will never call you for bank information', '2021-02-08');
+
+truncate table bank;
 
 insert into bank(bank_name)
 values ('HSBC'),
        ('BNP Paribas'),
        ('Standard Chartered');
+
+truncate table account;
 
 insert into account(account_no, balance, bank_id)
 values ('12345', '100000', '1'),
